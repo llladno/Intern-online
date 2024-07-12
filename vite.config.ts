f6f1187e5894dbeacb5bf20229244ea18b0b3a3e
llladno <man30968@gmail.com>
2024-07-13 00:17:56 +0300

    feat: add shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
+import path from "path"
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
