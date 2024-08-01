fc75c44d94ad6de5a71a357dfa9e1044043301e0
llladno <llladno@users.noreply.github.com>
2024-08-01 20:56:37 +0000

    deploy: 2c34cd7110f262af5790b48739b1ba9535f7569d

---
+++ assets/IOButton-BaG5hIwE.js
@@ -0,0 +1,1 @@
+import{d as o,c as e,s as n,n as s,o as a,_ as l}from"./index-WkCtGsB_.js";const u=o({__name:"IOButton",props:{fullWidth:{type:Boolean},outlined:{type:Boolean}},setup(r){return(t,p)=>(a(),e("button",{class:s({"w-full":t.fullWidth,button:!0,"button--outlined":t.outlined})},[n(t.$slots,"default",{},void 0,!0)],2))}}),c=l(u,[["__scopeId","data-v-3e278ac6"]]);export{c as I};
